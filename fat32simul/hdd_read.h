37ab9fd811e6b82116e49fe7874e20b937ded70c
xahigoyez <pierre.bertin-johannet@orange.fr>
2017-06-29 00:51:00 +0200

    added clearer comments

--- fat32simul/hdd_read.h
+++ fat32simul/hdd_read.h
@@ -8,6 +8,10 @@
  * File:   hdd_read.h
  * Author: user
  *
+ * This file is used to emulate the Read/Write sector interuptions availables in
+ * the protected mode.
+ * these two functions are the only ways the explorer will access the hdd variable.
+ * 
  * Created on 24 juin 2017, 14:44
  */
 
@@ -20,7 +24,23 @@ extern "C" {
 #endif
     unsigned char* hdd;
     typedef unsigned char* sector;
+    /**
+     * emulates a function tha could use the int13h bios interuption to write the
+     * content of a sector into memory and returns a pointer to it.
+     * @param sec : the adress (LBA) of the sector to write.
+     * @return 
+     */
+    sector read_sector(int sec);
+    /**
+     * writes the values of the given sector at the desired place in the disk.
+     * @param sec : the LBA adress of the sector to write.
+     * @param to_write : the array of bytes to write.
+     */
     void write_sector(int sec, sector to_write);
+    /**
+     * frees the array containg the bytes.
+     * @param sec
+     */
     void delete_sector(sector sec);
 #ifdef __cplusplus
 }
--- fat32simul/helper.h
+++ fat32simul/helper.h
@@ -20,13 +20,38 @@ extern "C" {
 #include "hdd_read.h"
 #include <stdio.h>
 #include <stdlib.h>
-    
+    /**
+     * reads a string at a specific offset in the byte array.
+     * @param sec : the sector/byte array 
+     * @param offset the offset of the string.
+     * @param size : the size of the string.
+     * @return the string found.
+     */
     unsigned char* read_name(sector sec, int offset, int size);
+    /**
+     * read an integer at a specific offset in the byte array. (Big endian)
+     * @param sec : the sector/byte array
+     * @param offset : the offset of the integer.
+     * @param size : the size in bytes of the integer.
+     * @return the integer
+     */
     int read_integer(sector sec, int offset, int size);
+    /**
+     * Writes a string in the sector.
+     * @param sec : the sector.
+     * @param name : the string to write.
+     * @param offset : the offset to write at.
+     * @param size : the size of the string.
+     */
     void write_name(sector sec, char* name, int offset, int size);
+    /**
+     * writes an integer in the sector.
+     * @param sec : the sector.
+     * @param integer : the integer to write.
+     * @param offset : the offset to write at.
+     * @param size : the size of the integer.
+     */
     void write_integer(sector sec, int integer, int offset, int size);
-
-
 
 #ifdef __cplusplus
 }
